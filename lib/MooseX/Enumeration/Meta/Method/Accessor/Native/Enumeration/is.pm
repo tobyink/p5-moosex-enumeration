1d770f6b7156cade381805048818200e134db8bb
Toby Inkster <mail@tobyinkster.co.uk>
2014-05-18 09:45:41 +0100

    this beats Moose's native accessor, and closes the gap slightly on XSAccessor

--- lib/MooseX/Enumeration/Meta/Method/Accessor/Native/Enumeration/is.pm
+++ lib/MooseX/Enumeration/Meta/Method/Accessor/Native/Enumeration/is.pm
@@ -12,10 +12,33 @@ with 'Moose::Meta::Method::Accessor::Native::Reader';
 sub _minimum_arguments { 1 }
 sub _maximum_arguments { 1 }
 
-sub _return_value {
+sub _return_value
+{
 	my $self = shift;
 	my ($slot_access) = @_;
 	return $slot_access . ' eq $_[0]';
 }
+
+around _generate_method => sub
+{
+	my $next = shift;
+	my $self = shift;
+	
+	my @curried = @{ $self->curried_arguments };
+	
+	if (@curried==1 and defined $curried[0] and not ref $curried[0])
+	{
+		require B;
+		require Moose::Util;
+		return sprintf(
+			'sub { @_ > 1 and %s; %s eq %s }',
+			"Moose::Util::throw_exception('MethodExpectsFewerArgs', 'method_name', 'is', 'maximum_args', 1)",
+			$self->_get_value('$_[0]'),
+			B::perlstring($curried[0]),
+		);
+	}
+	
+	$self->$next(@_);
+};
 
 1;
