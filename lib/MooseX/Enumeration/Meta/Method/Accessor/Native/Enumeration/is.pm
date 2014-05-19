4e446a06888b5b7a06160da57b65e324dc50558b
Toby Inkster <mail@tobyinkster.co.uk>
2014-05-19 10:05:33 +0100

    better comments; avoid role conflict in Moose older than 2.0800

--- lib/MooseX/Enumeration/Meta/Method/Accessor/Native/Enumeration/is.pm
+++ lib/MooseX/Enumeration/Meta/Method/Accessor/Native/Enumeration/is.pm
@@ -9,13 +9,15 @@ our $VERSION   = '0.003';
 use Moose::Role;
 with 'Moose::Meta::Method::Accessor::Native::Reader';
 
-sub _minimum_arguments { 1 }
-sub _maximum_arguments { 1 }
+around _minimum_arguments => sub { 1 };
+around _maximum_arguments => sub { 1 };
 
 sub _return_value
 {
 	my $self = shift;
 	my ($slot_access) = @_;
+	# Note that $_[0] comes from @curried which has been closed over
+	# and contains the string we need to compare against.
 	return $slot_access . ' eq $_[0]';
 }
 
@@ -26,18 +28,26 @@ around _generate_method => sub
 	
 	my @curried = @{ $self->curried_arguments };
 	
-	if (@curried==1 and defined $curried[0] and not ref $curried[0])
+	# If everything is as expected...
+	if ( @curried==1
+	and defined $curried[0]
+	and not ref $curried[0]
+	and $self->_maximum_arguments==1
+	and $self->_minimum_arguments==1 )
 	{
+		# ... then provide a highly optimized accessor.
 		require B;
 		require Moose::Util;
 		return sprintf(
-			'sub { @_ > 1 and %s; %s eq %s }',
+			'sub { %s if @_ > 1; %s eq %s }',
 			"Moose::Util::throw_exception('MethodExpectsFewerArgs', 'method_name', 'is', 'maximum_args', 1)",
 			$self->_get_value('$_[0]'),
 			B::perlstring($curried[0]),
 		);
 	}
 	
+	# Otherwise we should trust the default implementation
+	# from Moose::Meta::Method::Accessor::Native::Reader.
 	$self->$next(@_);
 };
 
